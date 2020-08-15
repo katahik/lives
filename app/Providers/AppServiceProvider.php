885c9742b9bf1265cbf210b0c48075f4da659c40
kotakatahira <koutakatahira0@gmail.com>
2020-08-16 06:21:01 +0900

    このサイトにアクセスできません画面が表示されるためコメントアウト

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+//        このサイトにアクセスできません画面が表示されるためコメントアウト
+//        \URL::forceScheme('https');
     }
 }

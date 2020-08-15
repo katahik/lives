856f07448e5aa0645321e8a4bd1ed570681e77a3
kotakatahira <koutakatahira0@gmail.com>
2020-08-16 05:55:00 +0900

    会員登録のルート作成

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');

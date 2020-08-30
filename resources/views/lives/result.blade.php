c94ed7de6e38478bde599c8ecd2cfda22a895fcc
kotakatahira <koutakatahira0@gmail.com>
2020-08-30 10:04:53 +0900

    検索画面にて、検索結果ともう一度検索するフォーム欄を入れ替えた

--- resources/views/lives/result.blade.php
+++ resources/views/lives/result.blade.php
@@ -5,64 +5,66 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8" id="map" style="height:500px"></div>
-                    <div class="offset-md-1 col-md-3">
+                    <div class="col-md-4">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>title</th>
+                                <th>アーティスト</th>
+                                <th>会場</th>
+                                <th>日にち</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($lives as $live)
+                                <tr>
+                                    <td>{!! link_to_route('lives.show', $live->title, ['live' => $live->id]) !!}</td>
+                                    <td>{{ $live->artist }}</td>
+                                    <td>{{ $live->venue }}</td>
+                                    <td>{{ $live->date }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>もう一度検索する</h2>
                         {!! Form::open(['route' => 'lives.result','method' => 'get']) !!}
 
                         <div class="form-group row">
-                            {!! Form::label('freeword', 'フリーワード:') !!}
-                            {!! Form::text('freeword', old('title'), ['class' => 'form-control','placeholder' => 'フリーワード']) !!}
+                            {!! Form::label('freeword', 'フリーワード:',['class'=>"col-2 col-form-label"]) !!}
+                            {!! Form::text('freeword', old('title'), ['class' => 'col-10 form-control','placeholder' => 'フリーワード']) !!}
                         </div>
 
                         <div class="form-group row">
-                            {!! Form::label('date', '日にち:',['class'=>"col-form-label date"]) !!}
-                            {!! Form::date('date', \Carbon\Carbon::now(), ['class' => 'form-control']) !!}
+                            {!! Form::label('date', '日にち:',['class'=>"col-2 col-form-label date"]) !!}
+                            {!! Form::date('date', \Carbon\Carbon::now(), ['class' => 'col-10 form-control']) !!}
                         </div>
 
                         {{--            デフォルトで現在地を選択した状態で検索する--}}
                         <div class="form-group row">
-                            {!! Form::label('location', '場所:',['class'=>"col-form-label"]) !!}
-                            {!! Form::text('location', old('location'), ['class' => 'form-control' ,'placeholder' => '現在地周辺']) !!}
+                            {!! Form::label('location', '場所:',['class'=>"col-2 col-form-label"]) !!}
+                            {!! Form::text('location', old('location'), ['class' => 'col-10 form-control' ,'placeholder' => '現在地周辺']) !!}
                         </div>
 
                         {{--            デフォルトで全カテゴリーを設定した状態で検索する--}}
                         <div class="form-group row">
-                            {!! Form::label('category', 'カテゴリー:',['class'=>"col-form-label"]) !!}
-                            {!! Form::select('category', ['ロック','ポップス','パンク'] ,old('category'), ['class' => 'form-control' ,'placeholder' => 'すべてのカテゴリー']) !!}
+                            {!! Form::label('category', 'カテゴリー:',['class'=>"col-2 col-form-label"]) !!}
+                            {!! Form::select('category', ['ロック','ポップス','パンク'] ,old('category'), ['class' => 'col-10 form-control' ,'placeholder' => 'すべてのカテゴリー']) !!}
                         </div>
 
                         {!! Form::submit('検索',['class' => 'btn btn-primary']) !!}
 
-                        {!! Form::close() !!}</div>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
 
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>title</th>
-                    <th>日にち</th>
-                    <th>会場</th>
-                    <th>アーティスト</th>
-                    <th>チケット最低値</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($lives as $live)
-                    <tr>
-                        <td>{!! link_to_route('lives.show', $live->title, ['live' => $live->id]) !!}</td>
-                        <td>{{ $live->date }}</td>
-                        <td>{{ $live->venue }}</td>
-                        <td>{{ $live->artist }}</td>
-                        <td>{{ $live->min_price }}
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-{{--緯度経度の表示は必要ないためコメント--}}
-{{--            <table>--}}
-{{--                <tr><th>緯度</th><td id="table_lat"></td></tr>--}}
-{{--                <tr><th>経度</th><td id="table_lng"></td></tr>--}}
-{{--            </table>--}}
 @endsection
 
 @section('script')

28a0518e4d3bdf92cb07858dad2489b9d0b13ae6
kotakatahira <koutakatahira0@gmail.com>
2020-08-30 15:18:57 +0900

    カレンダー実装中

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -38,4 +38,30 @@
             </tr>
             @endforeach
     @endif
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                @foreach (['日', '月', '火', '水', '木', '金', '土'] as $dayOfWeek)
+                    <th>{{ $dayOfWeek }}</th>
+                @endforeach
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($dates as $date)
+                @if ($date->dayOfWeek == 0)
+                    <tr>
+                        @endif
+                        <td
+                            @if ($date->month != $currentMonth)
+                            class="bg-secondary"
+                            @endif
+                        >
+                            {{ $date->day }}
+                        </td>
+                        @if ($date->dayOfWeek == 6)
+                    </tr>
+                @endif
+            @endforeach
+            </tbody>
+        </table>
 @endsection

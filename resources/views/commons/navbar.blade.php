eb8ec93eda5da9c4ae1c03e9b0e567d1004d583a
kotakatahira <koutakatahira0@gmail.com>
2020-09-06 06:21:08 +0900

    navbarとりあえず完成

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,40 +1,32 @@
 <header class="mb-4">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-
-
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="collapse navbar-collapse" id="nav-bar">
-            <ul class="navbar-nav container">
+            <a class="navbar-brand js-scroll-trigger" href="/">Lives</a>
+            <ul class="navbar-nav text-uppercase ml-auto">
                 @if (Auth::check())
                     @can('system-only') {{-- システム管理者権限のみに表示される --}}
-{{--            ここにユーザー一覧リンクが入る--}}
-{{--                    <li class="nav-item"><a class="nav-link" href="">ユーザー一覧</a></li>--}}
                     <li class="nav-item">{!!link_to_route('users.index', 'ユーザー一覧', [],['class' => 'nav-link']) !!}</li>
 
                     @endcan
                     @can('admin-higher')　{{-- 管理者権限以上に表示される --}}
                         <li class="nav-item">{!!link_to_route('lives.index', 'ライブ一覧', [],['class' => 'nav-link']) !!}</li>
-{{--            ここにライブ一覧リンクが入る--}}
                     @endcan
                     @can('user-higher') {{-- 一般権限以上に表示される --}}
-                {{-- トップページへのリンク --}}
-                    <li class="nav-item">{!!link_to_route('signup.get', 'Lives', [],['class' => 'nav-link']) !!}</li>
-                {{-- 行ったライブへのリンク--}}
+                    {{-- 行ったライブへのリンク--}}
                     <li class="nav-item">{!! link_to_route('users.show', '行ったライブ', ['user' => Auth::id()],['class' => 'nav-link']) !!}</li>
-                {{--ログアウトボタン--}}
+                    {{--ログアウトボタン--}}
                     <li class="nav-item">{!! link_to_route('logout', 'ログアウト', ['user' => Auth::id()],['class' => 'nav-link']) !!}</li>
                     @endcan
                 @else
-                    {{-- トップページへのリンク --}}
-                    <li class="nav-item col-md-4"><a href="/" class="nav-link">Lives</a></li>
-                    {{-- 行ったライブへのリンク--}}
-{{--                行ったライブへのリンクはログインしていないとログイン画面へ遷移する--}}
-                    <li class="nav-item col-md-4">{!!link_to_route('login', '行ったライブ', [], ['class' => 'nav-link']) !!}</li>
+                    {{--行ったライブへのリンクはログインしていないとログイン画面へ遷移する--}}
+                    <li class="nav-item">{!!link_to_route('login', '行ったライブ', [], ['class' => 'nav-link']) !!}</li>
                     {{-- ログインページへのリンク --}}
-                    <li class="nav-item col-md-4">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
                     {{-- 会員登録へのリンク --}}
-                    <li class="nav-item col-md-4">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                 @endif
             </ul>
         </div>
     </nav>
 </header>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <title>Lives</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link href="assets/css/styles.css" rel="stylesheet" />
 </head>
 
 <body>

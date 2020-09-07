9d4e94df838746f866913e74ae35d57aeaa84cc9
kotakatahira <koutakatahira0@gmail.com>
2020-09-08 05:34:33 +0900

    debug　検索がうまくされていなかった

--- resources/views/commons/navbar2.blade.php
+++ resources/views/commons/navbar2.blade.php
@@ -7,7 +7,6 @@
                     <div class="row align-items-center">
                         <!-- Logo -->
                         <div class="col-xl-2 col-lg-2 col-md-1">
-
                             <div class="logo">
                                 <a href="/">Lives</a>
                             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,6 +51,13 @@
 </div>
 
 <!-- JS here -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+@yield('script')
+
+
 <!-- All JS Custom Plugins Link Here here -->
 <script src="/assets/js/vendor/modernizr-3.5.0.min.js"></script>
 <!-- Jquery, Popper, Bootstrap -->

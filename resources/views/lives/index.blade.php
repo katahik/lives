4a46b4e6359cc47c0a9aefa6a541d0042a766725
kotakatahira <koutakatahira0@gmail.com>
2020-09-09 20:19:53 +0900

    >>>の削除

--- resources/views/lives/index.blade.php
+++ resources/views/lives/index.blade.php
@@ -10,7 +10,6 @@
         <!--チェックをつけたライブの削除-->
         {!! Form::model($lives, ['route' => ['lives.destroy'], 'method' => 'delete']) !!}
         {!! Form::submit('チェックを入れたライブを削除する', ['class' => 'btn btn-danger']) !!}
-
         {!! link_to_route('lives.create', '新規ライブの作成', [], ['class' => 'btn btn-primary']) !!}
 
         @if (count($lives) > 0)
@@ -24,9 +23,8 @@
                 <th>会場</th>
                 <th>カテゴリー</th>
                 <th>アーティスト</th>
-                <th>チケット最低値</th>
-                <th>チケット最高値</th>
-                <th>公式URL</th>
+                <th>Min_fee</th>
+                <th>Max_fee</th>
                 <th>作成したuser_id</th>
                 <th>ライブイメージ</th>
                 <th>更新日</th>
@@ -36,7 +34,7 @@
             @foreach ($lives as $live)
             <tr>
                 <td><input type="checkbox" name="ids[]" value="{{ $live->id }}"></td>
-                <td>{!! link_to_route('lives.show', $live->id, ['live' => $live->id]) !!}</td>>
+                <td>{!! link_to_route('lives.show', $live->id, ['live' => $live->id]) !!}</td>
                 <td>{{ $live->title }}</td>
                 <td>{{ $live->date }}</td>
                 <td>{{ $live->venue }}</td>
@@ -44,7 +42,6 @@
                 <td>{{ $live->artist }}</td>
                 <td>{{ $live->min_price }}</td>
                 <td>{{ $live->max_price }}</td>
-                <td>{{ $live->url }}</td>
                 <td>{{ $live->user_id }}</td>
                 <td>{{ $live->live_image }}</td>
                 <td>{{ $live->updated_at }}</td>

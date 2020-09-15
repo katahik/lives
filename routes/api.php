01c7fb60110efdc0a17289b9ffd291824bee932f
Ubuntu <ubuntu@ip-172-31-36-216.ap-northeast-1.compute.internal>
2020-09-15 11:50:11 +0000

    api修正

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,6 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', 'MyController@user');
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
